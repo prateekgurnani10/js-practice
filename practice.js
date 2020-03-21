f8311578fc8c8410984f9113fc3384809579ccc6
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-03-21 18:14:16 -0400

    practiced

---
+++ practice.js
@@ -0,0 +1,25 @@
+const number = 100;
+
+!!(number);
+
+const foo = 'string';
+
+foo.split(' ');
+console.log(foo);
+
+const hello = 'Prateek#100#hi'
+
+const emoji = 'cake💔';
+
+const zoo = {
+    lion : '🦁',
+    panda : '🐼'
+};
+
+console.log(Object.keys(zoo));
+console.log(Object.values(zoo));
+console.log(Object.entries(zoo));
+
+const bar = ['🍫' ,'🍬' ,'🎂 '];
+console.log(bar.includes('🐟'));
+
